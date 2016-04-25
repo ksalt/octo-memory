9f484f79101437bfa32b116e98203e2ae4e18485
Konstantyn Saltovets <konstantyn.sk@gmail.com>
2016-04-25 16:45:05 +0300

    created base layout

---
+++ app/components/card-section.component.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var React = require('react');
+
+let CardSection = React.createClass({
+  render() {
+    return (
+      <div className="card-container"></div>
+    );
+  }
+});
+
+module.exports = CardSection;
+
--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+require("./assets/styles/main.css");
+var files = require.context('./assets/svg', false, /\.svg$/);
+files.keys().forEach(files);
+
 var ReactDOM = require('react-dom'),
   React = require('react'),
   OctoMemory = require('./components/octomemory.component');
