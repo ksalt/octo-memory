97d2b461599025db9c3f4f3dbb244b6690eabd41
Konstantyn Saltovets <konstantyn.sk@gmail.com>
2017-07-27 18:42:37 +0300

    webpack production config tmp

---
+++ webpack.production.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].[contenthash].css",
+  disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "[name]-[hash].js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader?' + JSON.stringify({
+          name: '[name]',
+          prefixize: false
+        })
+      },
+      {
+        test: /\.(gif|png|woff|woff2|eot|ttf|fog|otf)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [
+      path.join('./app'),
+      "node_modules"
+    ],
+    extensions: ['.js', '.json']
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    extractSass
+  ]
+};
