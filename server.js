a22d5fe56a0b6d1351700761a48a49b73a5f1c72
Konstantyn Saltovets <konstantyn.sk@gmail.com>
2017-08-25 15:10:46 +0300

    server config

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/build'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
