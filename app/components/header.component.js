7e10e1063293c30ad6cb2614e576ca35e3cabddb
Konstantyn Saltovets <konstantyn.sk@gmail.com>
2017-08-26 16:36:30 +0300

    icons

--- app/components/header.component.js
+++ app/components/header.component.js
@@ -14,7 +14,7 @@ export default React.createClass({
       <div className="header">
         <div className="header__title">Octo-Memory</div>
         <div className="header__settings" onClick={this.showSettings}>
-          <Icon width="32" height="32" icon="ic_settings_black_24px" />
+          <Icon width="32" height="32" icon="settings" />
         </div>
       </div>
     );
